e8eda1734bae7c3177b9d1d4643c4aa5d0a010b8
Tobias Ortmayr <tortmayr@eclipsesource.com>
2020-08-15 18:31:59 +0200

    #98 Migrate theia-dependent styles from @eclipse-glsp/client
    Part of eclipse-glsp/glsp/issues/98

--- src/browser/language/glsp-client-contribution.ts
+++ src/browser/language/glsp-client-contribution.ts
@@ -13,6 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
+import "../../../css/decoration.css";
 import "../../../css/diagram.css";
 import "../../../css/theia-dialogs.css";
 import "../../../css/tool-palette.css";
