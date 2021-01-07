bff09dcdbdba5f9140bac4567e75ec4fed3c2d51
Jonas Helming <jhelming@eclipsesource.com>
2021-01-07 16:14:08 +0100

    Update examples/workflow-theia/server/download.ts
    Co-authored-by: Philip Langer <planger@users.noreply.github.com>

--- examples/workflow-theia/server/download.ts
+++ examples/workflow-theia/server/download.ts
@@ -26,5 +26,5 @@ const classifier = "glsp";
 console.log("Downloading latest version of the Workflow Example Java Server from the maven repository...");
 download({ groupId, artifactId, version, classifier, isSnapShot: true }, downloadDir, mavenRepository)
     .then(() => console.log("Download completed. Start the Theia back-end using these commands: \ncd examples/browser-app\nyarn start\n\n"
-        + "After starting the server, access the following link locally in your browser to see the running example:\n"
+        + "After starting the Theia back-end, access the following link locally in your browser to see the running example:\n"
         + "http://localhost:3000"));
