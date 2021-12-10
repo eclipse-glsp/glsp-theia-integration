65d5c6bd41fd8684efe6ef5e4e5300fda44b0653
Tobias Ortmayr <tortmayr@eclipsesource.com>
2021-12-10 01:28:35 +0100

    Switch GLSP client dependencies back to next

--- examples/workflow-theia/server/download.ts
+++ examples/workflow-theia/server/download.ts
@@ -17,14 +17,14 @@ import download from 'mvn-artifact-download';
 import { join, resolve } from 'path';
 
 const downloadDir = resolve(join(__dirname));
-const mavenRepository = 'https://oss.sonatype.org/content/groups/public/';
+const mavenRepository = 'https://oss.sonatype.org/content/repositories/snapshots/';
 const groupId = 'org.eclipse.glsp.example';
 const artifactId = 'org.eclipse.glsp.example.workflow';
-const version = '0.9.0';
+const version = '0.10.0';
 const classifier = 'glsp';
 
 console.log('Downloading latest version of the Workflow Example Java Server from the maven repository...');
-download({ groupId, artifactId, version, classifier, isSnapShot: false }, downloadDir, mavenRepository).then(() =>
+download({ groupId, artifactId, version, classifier, isSnapShot: true }, downloadDir, mavenRepository).then(() =>
     console.log(
         'Download completed. Start the Theia back-end using these commands: \ncd examples/browser-app\nyarn start\n\n' +
             'After starting the Theia back-end, access the following link locally in your browser to see the running example:\n' +
